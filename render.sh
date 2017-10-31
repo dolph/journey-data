c5917feaf4577ca5457f85cdace4dbbbff31c350
Dolph Mathews <dolph.mathews@gmail.com>
2017-10-31 16:09:04 -0500

    CI test for latest index.html
    This re-runs render.sh in Travis, and tests to see if that results in a
    diff of the repo, failing the Travis job if so.
    
    j2cli does not appear to support Python 3.6, so this patch also reverts
    the Travis environment to 2.7:
    
        /home/travis/virtualenv/python3.6.3/bin/j2
        Traceback (most recent call last):
        File "/home/travis/virtualenv/python3.6.3/bin/j2", line 11, in <module>
            sys.exit(main())
        File "/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/j2cli/cli.py", line 124, in main
            sys.argv[1:]
        File "/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/j2cli/cli.py", line 114, in render_command
            context
        File "/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/j2cli/cli.py", line 53, in render_template
            .get_template(template_path) \
        File "/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/jinja2/environment.py", line 830, in get_template
            return self._load_template(name, self.make_globals(globals))
        File "/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/jinja2/environment.py", line 804, in _load_template
            template = self.loader.load(self, name, globals)
        File "/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/jinja2/loaders.py", line 113, in load
            source, filename, uptodate = self.get_source(environment, name)
        File "/home/travis/virtualenv/python3.6.3/lib/python3.6/site-packages/j2cli/cli.py", line 26, in get_source
            contents = f.read().decode(self.encoding)
        AttributeError: 'str' object has no attribute 'decode'

--- render.sh
+++ render.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-which j2 || (echo "Run 'pip install -r requirements.txt' first" && exit 1)
+which j2 > /dev/null 2>&1 || (echo "Run 'pip install -r requirements.txt' first" && exit 1)
 j2 index.j2 journeys.yaml > index.html
